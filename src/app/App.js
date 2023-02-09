cb7469997e6c1cc8d0a227b703402d991d20b159
PrabhashDiss <prabhashdissanayake2k@gmail.com>
2023-02-09 09:55:36 +0530

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -24,8 +24,8 @@ export default function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <div className="cursor__dot">
         <AnimatedCursor
-          innerSize={15}
-          outerSize={15}
+          innerSize={10}
+          outerSize={10}
           color="255, 255 ,255"
           outerAlpha={0.4}
           innerScale={0.7}
